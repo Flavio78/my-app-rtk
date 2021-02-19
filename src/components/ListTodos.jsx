7703756cecaf0738c159f50bba87d80ae9edebe8
Flavio78 <you@example.com>
2021-02-19 09:06:04 +0100

    small fixes

--- src/components/ListTodos.jsx
+++ src/components/ListTodos.jsx
@@ -5,21 +5,36 @@ import {
   ListItemIcon,
   Checkbox,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import { useSelector } from "react-redux";
 import { todoSelectors } from "../slices/todosSlice";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    color: theme.palette.grey,
+  },
+}));
+
 const ListTodos = () => {
+  const classes = useStyles();
   const allTodos = useSelector(todoSelectors.selectAll);
   const todos = allTodos.map((todoItem) => (
     <ListItem key={todoItem.id}>
       <ListItemText primary={todoItem.todo} />
       <ListItemIcon>
-        <Checkbox edge="start" checked={todoItem.completed} />
+        <Checkbox checked={todoItem.completed} />
       </ListItemIcon>
     </ListItem>
   ));
-  return <List dense>{todos}</List>;
+  return (
+    <List dense className={classes.root}>
+      {todos}
+    </List>
+  );
 };
 
 export default ListTodos;
